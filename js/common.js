79c6f186c715bc30552449beb3dc621b110ce2d2
joodi8 <8jooj8@gmail.com>
2022-02-08 14:28:27 +0900

    up

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 $ (function(){
-    var gnbFlag = false;
-    var gnbIndex;
+    let gnbFlag = false;
+    let gnbIndex;
     
     $(".gnb_anchor a").on({
         "mouseenter" : function() {
@@ -17,19 +17,18 @@ $ (function(){
             $("#lnb_box").show();
             
             $(".lnb").eq(gnbIndex).css("display","block");
-            
             // 액티브 바 보여주기
             $("#active_bar").show();
             
-            var anchorLeft = $(this).offset().left;
-            var anchorWidth = $(this).width();
+            let anchorLeft = $(this).offset().left;
+            let anchorWidth = $(this).width();
             
             $("#active_bar").width(anchorWidth + 40);
             $("#active_bar").offset({left: anchorLeft - 20});
         }
     });
     
-    $("#lnb_box").on({
+    $("#lnb_box a").on({
         "mouseleave" : function() {
             if(gnbFlag) {
                 $("#lnb_box").removeClass("appear")
@@ -42,17 +41,26 @@ $ (function(){
         }
     });
 });
-    
-// let anchorUnderLine = document.getElementById("active_bar");
-// let   =  document.querySelectorAll(".gnb_anchor a")
-// console.log(anchorMenu);
 
-// anchorMenu.foEach (menu=>menu.addEventListener("mouseenter", (e) => anchorIndicator(e)))
 
-// function anchorIndicator (e) {
-//     anchorUnderLine.style.left = e.currentTarget.offsetLeft + "px";
-//     anchorUnderLine.style.Width = e.currentTarget.offsetWidth + "px";
-//     anchorUnderLine.style.top = 
-//     e.currentTarget.offsettop + e.currentTarget.offsetheight + "px";
-    
-// }
+// $(function(){
+//     $(window).resize(function(){
+//         let windowW = $(window).width();
+//         if (windowW <= 1023) {
+//             $(function() {
+//                 $("#icon_box a:nth-child(4)").on({
+//                     "click" : function() {
+//                         $("#lnb_box").show();
+//                         $("#gnb_menu").show();
+//                         $(".lnb li").hide();
+            
+//                         let gnbMoveBox = $("#gnb_menu");
+//                         $("#lnb_innerBox").prepend(gnbMoveBox);
+//                     }
+//                 });
+//             });
+//         } else if (windowW > 1023) {
+//             stop;
+//         }
+//     });
+// });
